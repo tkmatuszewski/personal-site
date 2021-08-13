b2bf4e8de0d303532cddae712e68026da28bc315
Tomasz Matuszewski <tkmatuszewski@gmail.com>
2021-08-13 11:33:53 +0200

    Hero section added

---
+++ src/components/Hero.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const Hero = styled.section`
+    min-height : 100vh;
+    background : black;
+    display: grid;
+    grid-template-rows: 3.5% 93% 3.5%;
+    grid-template-columns: 3.5% 94% 3.5%;
+`
+export default Hero
