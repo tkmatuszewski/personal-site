32e33663fd7a76a0a852da39cae322e6d6b9f75a
Tomasz Matuszewski <tkmatuszewski@gmail.com>
2021-08-13 11:38:01 +0200

    component tree update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,24 @@
 import * as React from "react"
+import GlobalFonts from '../fonts/fonts';
 import GlobalStyle from "../components/GlobalStyle"
+import Hero from "../components/Hero"
+import Logo from "../components/Logo"
+import NavigationBtn from "../components/NavigationBtn"
+import { StaticImage } from "gatsby-plugin-image";
 
 const IndexPage = () => {
   return (
     <main>
+      <GlobalFonts/>
       <GlobalStyle/>
+      <Hero>
+          <Logo>
+            TM
+          </Logo>
+          <NavigationBtn/>
+          <HeroBackLight/>
+          <StaticImage></StaticImage>
+      </Hero>
     </main>
   )
 }
