65494e776c44890d211322b9bd0920369633867c
Tomasz Matuszewski <tkmatuszewski@gmail.com>
2021-08-13 11:34:36 +0200

    Logo added

---
+++ src/components/Logo.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Logo = styled.h1`
+    font-size : 1.5rem;
+    font-weight : 900;
+    color : white;
+    font-family: 'Raleway', sans-serif;
+    grid-column: 2/3;
+    grid-row: 2/3;
+`
+
+export default Logo;
