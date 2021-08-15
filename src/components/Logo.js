8f02a238ec94e4794f2db30f0d3a576866b4165e
Tomasz Matuszewski <tkmatuszewski@gmail.com>
2021-08-15 23:43:35 +0200

    export component corrected

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
+import React from "react";
 
-const Logo = styled.h1`
+const LogoStyled = styled.h1`
     font-size : 1.5rem;
     font-weight : 900;
     color : white;
@@ -8,5 +9,8 @@ const Logo = styled.h1`
     grid-column: 2/3;
     grid-row: 2/3;
 `
+function Logo () {
+    return <LogoStyled>TM</LogoStyled>
+}
 
 export default Logo;
