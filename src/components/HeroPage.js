d43c7be2b775195ca305f4e079c062ddc86b6e61
Tomasz Matuszewski <tkmatuszewski@gmail.com>
2021-08-20 00:32:03 +0200

    Hero component renamed

---
+++ src/components/HeroPage.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const HeroPage = styled.section`
+    min-height : 100vh;
+    background : black;
+    display: grid;
+    grid-template-rows: 3.5% 93% 3.5%;
+    grid-template-columns: 3.5% 94% 3.5%;
+`
+export default HeroPage
