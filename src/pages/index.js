aeeddb6592bb504330bd703816ef8fe037b36e3a
Tomasz Matuszewski <tkmatuszewski@gmail.com>
2021-08-20 00:32:35 +0200

    Component import update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,22 @@
 import * as React from "react"
 import GlobalFonts from '../fonts/fonts';
 import GlobalStyle from "../components/GlobalStyle"
-import Hero from "../components/Hero"
+import HeroPage from "../components/HeroPage"
 import Logo from "../components/Logo"
-import NavigationBtn from "../components/NavigationBtn"
-import { StaticImage } from "gatsby-plugin-image";
+import HeroBackLight from "../components/HeroBackLight"
+import Hamburger from "../components/Hamburger";
 
 const IndexPage = () => {
+
   return (
     <main>
       <GlobalFonts/>
       <GlobalStyle/>
-      <Hero>
-          <Logo>
-            TM
-          </Logo>
-          <NavigationBtn/>
-          <HeroBackLight/>
-          <StaticImage></StaticImage>
-      </Hero>
+      <HeroPage>
+        <Logo/>
+        <Hamburger />
+      </HeroPage>
+      {/* <HeroBackLight/> */}
     </main>
   )
 }
