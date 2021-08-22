79c78bf17d25838987ff3c81e5ece7305c8095a1
Tomasz Matuszewski <tkmatuszewski@gmail.com>
2021-08-23 01:08:12 +0200

    Logo-Intro compatible

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -2,15 +2,30 @@ import styled from "styled-components";
 import React from "react";
 
 const LogoStyled = styled.h1`
-    font-size : 1.5rem;
+    font-size : 3vh;
     font-weight : 900;
     color : white;
     font-family: 'Raleway', sans-serif;
     grid-column: 2/3;
     grid-row: 2/3;
+    transition: 0.5s;
 `
-function Logo () {
-    return <LogoStyled>TM</LogoStyled>
+function Logo ({introState}) {
+
+  
+    const handleLogoContent =(state)=> {
+        let content = "Tomasz Matuszewski"
+        if(!state) {
+           content = "TM"
+        }
+        return content;
+    }
+
+    return (
+    <LogoStyled>
+        {handleLogoContent(introState)}
+    </LogoStyled>
+    )
 }
 
 export default Logo;
