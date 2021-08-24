e8c3fecfef3ed69bb4a75fd2e720266b3ea30ed0
Tomasz Matuszewski <tkmatuszewski@gmail.com>
2021-08-25 00:36:19 +0200

    Merriweather font added, raleway font file names changed

--- src/fonts/fonts.js
+++ src/fonts/fonts.js
@@ -1,15 +1,19 @@
 import { createGlobalStyle } from 'styled-components';
 
-import raleway1 from './raleway.woff';
-import raleway2 from './raleway.woff2';
-
 export default createGlobalStyle`
     @font-face {
-        font-family: 'Font Name';
-        src: local('Font Name'), local('FontName'),
-        url(${raleway2 }) format('woff2'),
-        url(${raleway1 }) format('woff');
+        font-family: 'raleway';
+        src: url('./raleway.woff2') format('woff2'),
+             url('./raleway.woff') format('woff');
         font-weight: 300;
         font-style: normal;
     }
+    @font-face {
+    font-family: 'merriweatherregular';
+    src: url('merriweather-regular.woff2') format('woff2'),
+         url('merriweather-regular.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+
+}
 `;
