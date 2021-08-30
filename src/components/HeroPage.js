e22260c0e1aa0a85c49994ce5ef0602f0b083da0
Tomasz Matuszewski <tkmatuszewski@gmail.com>
2021-08-30 13:22:26 +0200

    Hero section redesigned

--- src/components/HeroPage.js
+++ src/components/HeroPage.js
@@ -1,10 +1,20 @@
 import styled from "styled-components";
+import React from "react";
 
-const HeroPage = styled.section`
-    min-height : 100vh;
-    background : black;
+const HeroPageStyled = styled.section`
+    min-height : 80vh;
+    margin-top: 20vh;
+    background : white;
     display: grid;
-    grid-template-rows: 3.5% 93% 3.5%;
-    grid-template-columns: 3.5% 94% 3.5%;
+    grid-template-columns: 5% 90% 5%;
 `
+
+const HeroPage = ({children})=> {
+    return (
+        <HeroPageStyled>
+            {children}
+        </HeroPageStyled>
+    )
+}
+
 export default HeroPage
