8fcd0e178a2517748e1a6b7f77c0be99e8ea5b88
Tomasz Matuszewski <tkmatuszewski@gmail.com>
2021-08-30 13:34:18 +0200

    Modifications update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,45 @@
-import * as React from "react"
+import React, {useState, useEffect} from "react";
 import GlobalFonts from '../fonts/fonts';
 import GlobalStyle from "../components/GlobalStyle"
 import HeroPage from "../components/HeroPage"
 import Logo from "../components/Logo"
-import HeroBackLight from "../components/HeroBackLight"
 import Hamburger from "../components/Hamburger";
+import HeroBars from "../components/HeroBars";
+import Footer from "../components/Footer";
+import Contact from "../components/Contact";
+import About from "../components/About"
+import Work from "../components/Work";
+import Header from "../components/Header";
 
 const IndexPage = () => {
 
+  const [intro, setIntro] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIntro(!intro);
+    }, 1000);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <main>
+    <>
       <GlobalFonts/>
       <GlobalStyle/>
-      <HeroPage>
-        <Logo/>
-        <Hamburger />
-      </HeroPage>
-      {/* <HeroBackLight/> */}
-    </main>
+      <Header>
+          <Logo introSatus={intro}/>
+          <Hamburger />
+        </Header>
+      <main>
+        <HeroPage>
+          <HeroBars/>
+        </HeroPage>
+        <About/>
+        <Work/>
+        <Contact/>
+      </main>
+      <Footer/>
+    </>
   )
 }
 
