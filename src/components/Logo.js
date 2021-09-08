ffa28f500192ec7bfca4bb8be7c9a7ebf041c1e1
Tomasz Matuszewski <tkmatuszewski@gmail.com>
2021-09-08 13:27:59 +0200

    Logo component colors issue fixed

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,22 +1,22 @@
 import styled from "styled-components";
-import React, { useEffect, useRef} from "react";
+import React from "react";
+
 
 
 const LogoStyled = styled.h1`
     grid-row: 1/2;
     grid-column: 2/3;
-    font-size : 3vh;
+    font-size : 2vh;
     font-weight : 900;
     font-family: 'Raleway', sans-serif;
     align-self: center;
-    transition: 0ms.5;
     opacity: 1;
 
     span {
         display: block;
     }
 `
-const Logo =()=> {
+const Logo = () => {
     
     return (
     <LogoStyled>
