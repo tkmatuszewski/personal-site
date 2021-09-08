43099d141e868d6499929928d31ebd8828a385c5
Tomasz Matuszewski <tkmatuszewski@gmail.com>
2021-09-08 13:30:51 +0200

    Page component imports updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,44 +1,41 @@
-import React, {useState, useEffect} from "react";
+import React, {useState} from "react";
 import GlobalFonts from '../fonts/fonts';
 import GlobalStyle from "../components/GlobalStyle"
 import HeroPage from "../components/HeroPage"
 import Logo from "../components/Logo"
-import Hamburger from "../components/Hamburger";
 import HeroBars from "../components/HeroBars";
 import Footer from "../components/Footer";
 import Contact from "../components/Contact";
 import About from "../components/About"
 import Work from "../components/Work";
 import Header from "../components/Header";
+import HeroImg from "../components/HeroImg";
+import NavigationBtn from "../components/NavigationBtn";
+import Menu from "../components/Menu";
 
 const IndexPage = () => {
 
-  const [intro, setIntro] = useState(true);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setIntro(!intro);
-    }, 1000);
-    return () => clearTimeout(timer);
-  }, []);
+  const [showMenu, setShowMenu] = useState(false);
 
   return (
     <>
       <GlobalFonts/>
       <GlobalStyle/>
-      <Header>
-          <Logo introSatus={intro}/>
-          <Hamburger />
+      <Menu showMenu = {showMenu} setShowMenu = {setShowMenu}/>
+        <Header>
+            <Logo/>
+            <NavigationBtn showMenu = {showMenu} setShowMenu = {()=>setShowMenu(!showMenu)}/>
         </Header>
-      <main>
-        <HeroPage>
-          <HeroBars/>
-        </HeroPage>
-        <About/>
-        <Work/>
-        <Contact/>
-      </main>
-      <Footer/>
+        <main>
+          <HeroPage>
+            <HeroBars/>
+            <HeroImg/>
+          </HeroPage>
+          <About/>
+          <Work/>
+          <Contact/>
+        </main>
+        <Footer/>
     </>
   )
 }
