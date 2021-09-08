a55e8aed5076a0980d00fb4da031ac1ca373733c
Tomasz Matuszewski <tkmatuszewski@gmail.com>
2021-09-08 13:23:18 +0200

    About updated

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
-import React from "react";
+import React, {useRef, useEffect} from "react";
+import  gsap from "gsap";
 
 const AboutStyled = styled.section`
 min-height: 100vh;
@@ -9,63 +10,145 @@ background: black;
 `
 
 const AboutCnt = styled.div`
-width: 65%;
+width: 50%;
 display: grid;
-grid-template-columns: 60% 40%;
-grid-template-rows: 1fr 1fr 1fr;
+grid-template-columns: 50% 50%;
+grid-template-rows: repeat(4, 40vh );
+grid-row-gap: 7vh;
 color: white;
+@media (max-width: 1000px) {
+  width: 80%;
+  grid-template-columns: 1fr;
+  grid-template-rows: repeat(4, 40vh);
+  grid-row-gap: 0;
+}
 `
 const AboutStrongCnt = styled.div`
 grid-column: 1/3;
 grid-row: 1/2;
-/* max-height: 30vh; */
-/* max-width: 70%; */
 margin-top: 15vh;
+margin-bottom: 15vh;
 z-index: 10;
+opacity: 0;
+@media (max-width: 1000px) {
+  width: 75%;
+  grid-template-columns: 1fr;
+  grid-template-rows: repeat(4, 40vh);
+  grid-row-gap: 0;
+}
+
 `
 const StrongRow = styled.strong`
-display: block;
-width: 100%;
-font-family: 'Raleway', sans-serif;
-font-weight: 600;
-font-size: 10vh;
+  display: block;
+  width: 100%;
+  font-family: 'Raleway', sans-serif;
+  font-weight: 600;
+  font-size: 10vh;
+  @media (max-width: 1000px) {
+    font-size: 5vh;
+      }
 `
 const AboutContent = styled.p`
-grid-column: 1/2;
-align-self: center;
-max-width: 75%;
-font-family: 'Merriweather', serif;
-font-size: 2.5vh;
-letter-spacing: 0.15em;
-`
+  grid-column: 2/3;
+  align-self: center;
+  max-width: 100%;
+  font-family: 'Merriweather', serif;
+  font-size: 2.5vh;
+  text-align: justify;
+  letter-spacing: 0.15em;
+  opacity: 0;
+  transform: translateX(-10vw);
+  @media (max-width: 1000px) {
+    max-width: 100%;
+    grid-column: 1/3;
+    align-self: center;
+      }
 
-const AboutImage = styled.div`
-height: 70vh;
-grid-column: 2/3;
-grid-row:1/4;
-align-self: center;
-background: darkgrey;
-z-index: 0;
+&:nth-of-type(1) {
+
+  align-self: flex-end;
+  @media (max-width: 1000px) {
+    max-width: 100%;
+  }
+}
+
+&:nth-of-type(2) {
+  grid-row: 3/4;
+  grid-column: 1/2;
+  transform: translateX(+7vw);
+}
+
+&:nth-of-type(3) {
+  grid-row: 4/5;
+  align-self: flex-start;
+}
 `
 
 const About = () => {
+
+    const AboutHeader = useRef()
+    const AboutColumn1 = useRef()
+    const AboutColumn2 = useRef()
+    const AboutColumn3 = useRef()
+
+      useEffect(() => {
+
+        gsap.to(AboutHeader.current,{scrollTrigger: {
+          trigger: ".About__AboutStrongCnt-eZoALK",
+          start: "center bottom",
+          end: "center center",
+          scrub: true,
+        }, x : "-=5vw", duration: "1",
+        alpha: "1",
+        ease: "circ.easeOut"});
+        gsap.to(AboutColumn1.current, { scrollTrigger: {
+            trigger: "#col1",
+            start: "center bottom",
+            end: "center center",
+            scrub: true,
+          }, duration: "1",
+          x: "+=10vw",
+          alpha: "1",
+          ease: "circ.easeOut"});
+        gsap.to(AboutColumn2.current, {  scrollTrigger: {
+            trigger: "#col2",
+            start: "center bottom",
+            end: "center center",
+            scrub: true,
+          }, duration: "1",
+          x: "-=7vw",
+          alpha: "1",
+          ease: "circ.easeOut"});
+        gsap.to(AboutColumn3.current, { scrollTrigger: {
+            trigger: "#col3",
+            start: "center bottom",
+            end: "center center",
+            scrub: true,
+          }, duration: "1",
+          x: "+=8vw",
+          alpha: "1",
+          ease: "circ.easeOut"});
+     }, []);
 
     return (
         <div id="about">
             <AboutStyled>
                 <AboutCnt>
-                    <AboutStrongCnt>
+                    <AboutStrongCnt ref={AboutHeader} id ="aboutHeader">
                         <StrongRow>I'm Tomasz,</StrongRow>
                         <StrongRow>a creative developer</StrongRow>
                         <StrongRow>based in Warsaw.</StrongRow>
                     </AboutStrongCnt>
-                    <AboutImage/>
-                    <AboutContent>
-                    I design and write apps and websites with great care for aesthetics. It is my ambition to create some meaningful and eye pleasing digital products.  
-                    Since I started my web development journey I try to constantly make even more engaging and performant stuff. 
+                    <AboutContent ref={AboutColumn1} id="col1">
+                    I design and write websites and apps with special care for aesthetics.  
+                    It is my ambition to create some meaningful and eye pleasing digital products.  
                     </AboutContent>
-                    <AboutContent>
-                        At the moment I am available for hire. I'm ready to jump into an awsome project.
+                    <AboutContent ref={AboutColumn2} id="col2">
+                    Since I started my web development journey a while ago I continue to improve patterns,
+                    technologies and techniques to make my work even more engaging and useful.
+                    </AboutContent>
+                    <AboutContent ref={AboutColumn3} id="col3">
+                        I am currently available for hire so write me and lets make a team.
                     </AboutContent>
                 </AboutCnt>
             </AboutStyled>
