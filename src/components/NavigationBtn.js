2c6ea1498baa93ac6ef3760b7af773d782d53556
Tomasz Matuszewski <tkmatuszewski@gmail.com>
2022-05-22 23:19:08 +0200

    NavigationBtn

--- src/components/NavigationBtn.js
+++ src/components/NavigationBtn.js
@@ -1,45 +1,41 @@
 import styled from "styled-components";
 import React, {useRef} from "react";
 
-const NavigationBtnStyled=styled.button`
-    grid-row: 1/2;
-    grid-column: 2/3;
-    justify-self: flex-end;
-    align-self: center;
-    height: 3.5vh;
-    background: transparent;
-    cursor: pointer;
-    font-family: 'Raleway', sans-serif;
-    font-size: 2.5vh;
-    border: none;
-    transform: translateX(-50%);
-    position: relative;
-    color: inherit;
-    z-index: 1;
+const NavigationBtnStyled = styled.button`
+  background: transparent;
+  cursor: pointer;
+  font-family: "Raleway", sans-serif;
+  font-size: 2.5vh;
+  font-weight: bold;
+  border: none;
+  position: relative;
+  z-index: 1;
+  color: inherit;
+  flex-grow: 0;
 
-    &:before {
-        content: "";
-        width: 0;
-        height: 2px;
-        background: black;
-        transition: 0.4s;
-        position: absolute;
-        bottom: -2%;
-        left: 0;
-    }
+  &:before {
+    content: "";
+    width: 0;
+    height: 2px;
+    background: black;
+    transition: 0.4s;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+  }
 
-    &:hover:before {
-        width: 100%;
-    }
-    &:hover:after {
-       height: 2vh;
-    }
-`
+  &:hover:before {
+    width: 100%;
+  }
+  &:hover:after {
+    height: 2vh;
+  }
+`;
 
 const NavigationBtn =({showMenu, setShowMenu})=> {
     
-    const menuBtn= useRef()
-
+  const menuBtn = useRef();
+  
     return (
         <NavigationBtnStyled ref={menuBtn} onClick={() => setShowMenu(!showMenu)}>
         Menu
