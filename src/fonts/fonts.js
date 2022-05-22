dd358dd044d949a72d6edffd550f6a8095fee769
Tomasz Matuszewski <tkmatuszewski@gmail.com>
2022-05-22 23:13:52 +0200

    font loading fix

--- src/fonts/fonts.js
+++ src/fonts/fonts.js
@@ -5,15 +5,13 @@ export default createGlobalStyle`
         font-family: 'raleway';
         src: url('./raleway.woff2') format('woff2'),
              url('./raleway.woff') format('woff');
-        font-weight: 300;
         font-style: normal;
     }
     @font-face {
     font-family: 'merriweatherregular';
-    src: url('merriweather-regular.woff2') format('woff2'),
-         url('merriweather-regular.woff') format('woff');
+    src: url('./merriweather-regular.woff2') format('woff2'),
+         url('./merriweather-regular.woff') format('woff');
     font-weight: normal;
     font-style: normal;
-
 }
 `;
